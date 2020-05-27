ba91e7c7c670b0eb3962974d2b3b8366dec3b696
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 17:15:37 +0200

    no comment

--- src/com/Xiaopi0/calculatorkotlinone/main.kt
+++ src/com/Xiaopi0/calculatorkotlinone/main.kt
@@ -8,7 +8,6 @@ fun main(args: Array<String>) {
     var loop = true
     while (loop == true) {
         println("Write two numbers to be added. -0 to quit(it doesn't matter where)")
-        //Create and initialize the variables for the numbers you put in
         val input_1 = scanner.nextInt()
         val input_2 = scanner.nextInt()
         if (input_1 == -0 || input_2 == -0) {
