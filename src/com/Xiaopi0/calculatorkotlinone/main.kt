5db118254507892cfee8469c3dfc5ca6ed2aa7ec
BuildTools <unconfigured@null.spigotmc.org>
2020-05-28 08:53:42 +0200

    changed how commands work

--- src/com/Xiaopi0/calculatorkotlinone/main.kt
+++ src/com/Xiaopi0/calculatorkotlinone/main.kt
@@ -7,14 +7,25 @@ fun main(args: Array<String>) {
 
     var loop = true
     while (loop == true) {
-        println("Write two numbers to be added. -0 to quit(it doesn't matter where)")
-        val input_1 = scanner.nextInt()
-        val input_2 = scanner.nextInt()
-        if (input_1 == -0 || input_2 == -0) {
+        println("Write two numbers to be added. ? to see list of commands")
+        val input_1 = scanner.next()
+        //Commands
+        if (input_1 == "?") {
+            println("List of commands:")
+            println()
+            println("1. ? = view this list.")
+            println("2. quit = quits the application.")
+            println()
+        } else if (input_1 == "quit") {
             loop = false
-        }else {
-            println(input_1 + input_2)
+        } else {
+            val input_2 = scanner.next()
+
+            val output_1 = input_1.toInt()
+            val output_2 = input_2.toInt()
+            println(output_1 + output_2)
             println()
         }
+
     }
 }
